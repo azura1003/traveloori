c134983e3113d326638e66cce9464d0fa132330c
azura1003 <noorkhan.95150@gmail.com>
2023-03-18 17:59:43 +0100

    ajout caroucel banner, ajout offres, et création du bouton logout

--- public/assets/js/banner.js
+++ public/assets/js/banner.js
@@ -1,15 +1,16 @@
-const bannerImages = [
-    '{{ asset('/img/atol-maldives.jpg') }}',
-    '{{ asset('/img/barca.jpg') }}',
-    '{{ asset('/img/atol-maldives.jpg') }}',
-    // Ajoutez d'autres images si nécessaire
-];
-  
-let currentImageIndex = 0;
+let slideIndex = 0;
+const banners = document.getElementsByClassName('banner');
 
-function changeBannerImage() {
-  currentImageIndex = (currentImageIndex + 1) % bannerImages.length;
-  document.querySelector('.banner').style.backgroundImage = `url('${bannerImages[currentImageIndex]}')`;
+function changeBanner() {
+  for (let i = 0; i < banners.length; i++) {
+    banners[i].classList.remove('active'); // On enlève la classe active de tous les éléments .banner
+  }
+  slideIndex++;
+  if (slideIndex > banners.length) {
+    slideIndex = 1;
+  }
+  banners[slideIndex - 1].classList.add('active'); // On ajoute la classe active à l'élément de la bannière qui est actuellement visible
+  setTimeout(changeBanner, 5000); // Change image every 3 seconds
 }
 
-setInterval(changeBannerImage, 3000);
+changeBanner();
